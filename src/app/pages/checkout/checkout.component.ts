b7f14429b3d04fa505a67bb3f331c429a7a7b9dd
Zain Ahmed <zaahmed@nisum.com>
2023-02-02 17:02:05 +0500

    working on ecommerce

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,3 +1,4 @@
+import { KeyValue } from '@angular/common';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loadStripe } from '@stripe/stripe-js';
@@ -12,20 +13,25 @@ import { addItemToCheckout } from 'src/app/model/actions/checkout.action';
 export class CheckoutComponent {
   checkout: any = {};
   products: any = {};
+  productKeys: any = [];
   orderComplete = false;
 
   constructor(private store: Store<{ checkout: Object }>) {
     store.select('checkout').subscribe((v: any) => {
       console.log(v);
       this.checkout = v;
-      this.products = { ...v.products };
+      if (Object.keys(v.products).length > 0) {
+        this.productKeys = Object.keys(v.products);
+        this.products = { ...v.products };
+      }
     });
   }
 
-  all = (item: any) => {
-    console.log(item);
-    return 1;
-  };
+  // funcFilter = (a: KeyValue<number, object>, b: KeyValue<number, object>) => {
+  //   console.log(a);
+  //   console.log(b);
+  //   return 0;
+  // };
 
   handleOrderConfirm = () => {
     this.orderComplete = !this.orderComplete;
